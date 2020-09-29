d0700a486001ed696d6dbe027eb0880ff04bdcb8
Jessica Stone <jstone4815@gmail.com>
2020-09-28 19:03:36 -0700

    remove comment and add Big O notation

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,10 +1,8 @@
 require_relative "min_heap"
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
-# With an array of n elements, to perform Heapsort you can add the elements to the heap, an O(nlog n) operation. Then you can remove the elements from the heap one-by-one placing them in the proper place in an array, also an O(nlog n) operation.
-
+# Time Complexity:  O(nlogn) - loops n times based on length of list, and then logn adding or removing
+# Space Complexity: O(nlogn)
 def heapsort(list)
   return list if list.length <= 1
 
