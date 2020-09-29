b46b13e93598bdef3bc6b4bbb5da89aa01241c4f
Jessica Stone <jstone4815@gmail.com>
2020-09-28 18:45:09 -0700

    clean up code and add big O notations

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -18,17 +18,19 @@ class MinHeap
     return (index - 1) / 2
   end
 
+  # helper method to find left child
   def left_child(index)
     return 2 * index + 1
   end
 
+  # helper method to find right child
   def right_child(index)
     return 2 * index + 2
   end
 
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(logn) depending on the height of the heap
+  # Space Complexity: O(logn) to hold the recursive stack
   def add(key, value = key)
     @store << HeapNode.new(key, value)
     heap_up(@store.length-1)
@@ -36,8 +38,8 @@ class MinHeap
 
   # This method removes and returns an element from the heap
   #   maintaining the heap structure
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(logn) depending on the height of the heap
+  # Space Complexity: O(logn) to hold the recursive stack
   def remove
     if @store.empty?
       return nil
@@ -76,16 +78,14 @@ class MinHeap
   # This helper method takes an index and
   #  moves it up the heap, if it is less than it's parent node.
   #  It could be **very** helpful for the add method.
-  # Time complexity: ?
-  # Space complexity: ?
+  # Time complexity: O(logn)
+  # Space complexity: O(logn)
   def heap_up(index)
     return if index == 0
     
     parent = find_parent(index)
     if @store[parent].key > @store[index].key
-      temp = @store[parent]
-      @store[parent] = @store[index]
-      @store[index] = temp
+      swap(parent, index)
       heap_up(parent)
     end
   end
@@ -94,16 +94,15 @@ class MinHeap
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    # compare the new root with its children and swap to maintain the heap order in an operation called heap-down. The heap down operation is repeated until a leaf node is reached or no swaps are made.
     return if index >= @store.length-1
 
     left = left_child(index)
     right = right_child(index)
-    smallest = nil
 
-    if !@store[left] && !@store[right]
-      return
-    elsif !@store[left]
+    return if !@store[left] && !@store[right]
+
+    smallest = nil
+    if !@store[left]
       smallest = right
     elsif !@store[right]
       smallest = left
@@ -115,14 +114,6 @@ class MinHeap
       swap(index, smallest)
       heap_down(smallest)
     end
-
-    # if  (right <= @store.length-1) &&@store[index].key > @store[right].key
-    #   swap(index, right)
-    #   heap_down(right)
-    # end
-
-    # heap_down(left)
-    # heap_down(right)
   end
 
   # If you want a swap method... you're welcome
